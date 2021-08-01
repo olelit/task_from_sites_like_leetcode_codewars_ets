ad8bc355911988d87262c4aa1c227766aaea9eba
oleg <litasov00@gmail.com>
2021-08-01 12:57:33 +0300

    add leetcode

---
+++ leetcode/two-sum.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+//leetcode живет немного в другом веке =)
+
+var twoSum = function (nums, target) {
+  for (let first = 0; first < nums.length; first++) {
+    let firstMapEl = nums[first];
+    for (let second = 0; second < nums.length; second++) {
+      let secondMapEl = nums[second];
+      if (first !== second) {
+        if (firstMapEl + secondMapEl === target) {
+          return [first, second];
+        }
+      }
+    }
+  }
+  return [];
+};
+
+//короче и читается лучше, но leetcode считает что это работает медленнее
+var twoSum2 = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let second = target - nums[i];
+    let secondIndex = nums.indexOf(second);
+    if (secondIndex !== -1 && secondIndex !== i) {
+      return [i, secondIndex];
+    }
+  }
+
+  return [];
+};
+
+const nums = [3, 3];
+const target = 6;
+
+console.log(twoSum(nums, target));
